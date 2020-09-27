bf5bc345be3aef222245a33da2cb07db2867a2c3
GitHub Action <action@github.com>
2020-09-27 06:01:01 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/hannesa2/gradleplugingenerategitchangelog/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hannesa2.gradleplugingenerategitchangelog.services
+
+import com.github.hannesa2.gradleplugingenerategitchangelog.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
